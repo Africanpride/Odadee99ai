12d115b621446b8ebf299facf36814ef474e32f5
Pius Opoku-Fofie <pius1919@gmail.com>
2024-06-03 09:52:20 +0000

    Gallery loaded ...

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -5,10 +5,15 @@ type Props = {};
 
 const page = (props: Props) => {
   // Generate dynamic image paths from "gallery15.jpg" to "gallery27.jpg"
-  const dynamicImagePaths = Array.from({ length: 13 }, (_, i) => ({
-    src: `/images/gallery/gallery${i + 15}.jpg`,
-    alt: `Gallery Image ${i + 15}`,
-    title: `Gallery Image ${i + 15}`,
+  const dynamicImagePaths = Array.from({ length: 7 }, (_, i) => ({
+    src: `/images/project/gallery${i +1}.jpg`,
+    alt: `Gallery Image ${i + 1}`,
+    title: `Gallery Image ${i + 1}`,
+  }));
+  const dynamicImagePaths2 = Array.from({ length: 28 }, (_, i) => ({
+    src: `/images/gallery/gallery${i +1}.jpg`,
+    alt: `Gallery Image ${i + 1}`,
+    title: `Gallery Image ${i + 1}`,
   }));
 
   return (
@@ -16,6 +21,19 @@ const page = (props: Props) => {
       <div className="masonry">
         {/* Render dynamic images */}
         {dynamicImagePaths.map((image, index) => (
+          <div className="brick" key={index}>
+            <Image
+              src={image.src}
+              alt={image.alt}
+              title={image.title}
+              width={500}
+              height={500}
+              sizes="(max-width: 768px) 100vw, 33vw"
+              className="rounded-3xl"
+            />
+          </div>
+        ))}
+        {dynamicImagePaths2.map((image, index) => (
           <div className="brick" key={index}>
             <Image
               src={image.src}
