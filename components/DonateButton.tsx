b8fdcee8c25c84428dd7f79991c8053844d98f23
Pius Opoku-Fofie <pius1919@gmail.com>
2024-06-03 10:03:49 +0000

    donation options edit ..

--- components/DonateButton.tsx
+++ components/DonateButton.tsx
@@ -1,7 +1,7 @@
 // Donation Process starts from here.
 import React, { useState } from "react";
 import { sourceCodePro400 } from "@/utils/styles/fonts";
-import { CircleChevronRight } from "lucide-react";
+import { CircleChevronRight, LucideSquareX } from "lucide-react";
 import Image from "next/image";
 
 type Props = {
@@ -51,21 +51,24 @@ const DonateButton: React.FC<Props> = (props: Props) => {
         >
           <div className="modal-box flex bg-none max-w-4xl p-0 relative overflow-hidden">
             <div className="w-full md:w-3/4 p-4 bg-white flex flex-col justify-between">
-              <h3 className="font-bold text-lg">
-                {props.title || "Odadee99AI Donation Options"}
-              </h3>
+              <div className="flex justify-between items-center">
+                <h3 className="font-bold text-lg">
+                  {props.title || "Odadee99AI Donation Options"}
+                </h3>
+                <LucideSquareX className="text-info hover:text-red-600 cursor-pointer " onClick={closeModal}/>
+              </div>
               <div className="py-4">
                 {props.children || "Link to Payment API (Paypal & Stripe)"}
               </div>
-              <div className="modal-action">
-                <button className="btn" onClick={closeModal}>
-                  Close 2
-                </button>
-              </div>
+
             </div>
             <div className="md:w-1/4 bg-yellow-600 relative object-center	rounded-r-2xl	">
-             <Image src="/images/kente.avif" alt="Ghana Kente donation" fill={true}
-             className="object-fill  absolute inset-0	" />
+              <Image
+                src="/images/kente.avif"
+                alt="Ghana Kente donation"
+                fill={true}
+                className="object-fill  absolute inset-0	"
+              />
             </div>
           </div>
         </dialog>
